fbd86306bbb44536e6a4d92dbddba5cb838e5434
46330019mercado <mercadonatali.epet20@gmail.com>
2022-10-06 13:06:28 -0300

    Primer ejercicio

---
+++ script.js
@@ -0,0 +1,8 @@
+//#region 
+var nombre1="El tiempo que se";
+console.log(nombre1);
+let nombre2="disfuta es el verdadero";
+console.log(nombre2);
+const nombre3="tiempo vivido";
+console.log(nombre3);
+//#endregion
